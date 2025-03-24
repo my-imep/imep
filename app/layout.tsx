85ccdba48d8f85205c22b4630ad6a16198c7eb4c
Rafael Dias <67400064+rafael-bit@users.noreply.github.com>
2025-03-24 15:20:32 -0300

    feat: fist configuration

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({
+  variable: "--font-roboto",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "IMEP - Igreja Missões do Evangelho Pleno",
+  description: "Site oficial da Igreja Missões do Evangelho Pleno",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body className={`${roboto.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
