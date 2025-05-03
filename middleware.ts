c757f36427f15f2ce83a0be06f4de85b68efe8e2
Rafael Dias <67400064+rafael-bit@users.noreply.github.com>
2025-05-03 10:41:43 -0300

    fix: base middleware improve

--- middleware.ts
+++ middleware.ts
@@ -9,17 +9,16 @@ const getUrl = (path: string) => {
 export async function middleware(request: NextRequest) {
 	const token = await getToken({ req: request as any, secret: process.env.NEXTAUTH_SECRET })
 	const pathname = request.nextUrl.pathname
+	const baseUrl = new URL('/', request.url).origin
 
 	if (pathname.startsWith('/api/')) {
 		const response = NextResponse.next()
 
-		// Add CORS headers
 		response.headers.set('Access-Control-Allow-Credentials', 'true')
 		response.headers.set('Access-Control-Allow-Origin', request.headers.get('origin') || '*')
 		response.headers.set('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
 		response.headers.set('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version')
 
-		// Handle preflight OPTIONS requests
 		if (request.method === 'OPTIONS') {
 			return new NextResponse(null, { status: 200, headers: response.headers })
 		}
@@ -37,19 +36,19 @@ export async function middleware(request: NextRequest) {
 
 	if (pathname === '/auth' && token) {
 		if (token.isAdmin) {
-			return NextResponse.redirect(new URL('/app', request.url))
+			return NextResponse.redirect(new URL('/app', baseUrl))
 		} else {
-			return NextResponse.redirect(new URL('/access-denied', request.url))
+			return NextResponse.redirect(new URL('/access-denied', baseUrl))
 		}
 	}
 
 	if (pathname.startsWith('/app')) {
 		if (!token) {
-			return NextResponse.redirect(new URL('/auth', request.url))
+			return NextResponse.redirect(new URL('/auth', baseUrl))
 		}
 
 		if (!token.isAdmin) {
-			return NextResponse.redirect(new URL('/access-denied', request.url))
+			return NextResponse.redirect(new URL('/access-denied', baseUrl))
 		}
 	}
 
